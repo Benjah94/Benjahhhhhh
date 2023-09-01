981c8f57562c8413c193416fd58cf5109e3b0409
Benjah94 <136342025+Benjah94@users.noreply.github.com>
2023-09-01 08:32:08 -0300

    Add files via upload

---
+++ Clase1.js
@@ -0,0 +1,62 @@
+//Punto 1
+cuadrado(a)
+ x^2-x-10
+{
+     return num * num
+}
+cuadrado(5)
+
+//Punto 2
+ (x-3)*2
+
+function cubo(num)
+{
+     return num * num * num
+}
+console.log(cubo(5))
+
+//Punto a
+x*x^2+20
+function operacionA(x)
+{
+     let resultado = cuadrado(x * x) - x - 10
+     return resultado
+}
+
+//punto b
+ (x/2)+5
+function operacionB(x)
+{
+let resultado = (x-3)*2
+return resultado
+}
+
+ x^3+x^2+10
+
+
+(x*10)*5
+
+//punto C
+function operacion(x)
+{
+let resultado = x * cuadrado(x) + 20
+return resultado
+} 
+//Punto d
+function operacionD(x)
+{
+     let resultado = (x/2) + 5
+     return resultado
+}
+// Punto e 
+function operacion(x)
+{
+     let resultado = cubo(x) + cuadrado(x) + 10
+     return resultado
+}
+//Punto F
+function operacionF(x)
+{
+     let resultado = (x*10) * 5
+     return resultado 
+}
